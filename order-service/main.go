e6971c2956cfb6398aff94744263c2d9671e38de
Giorgi Vanadze <giorgivanadze03@gmail.com>
2025-03-11 18:46:35 +0400

    Added RabbitMQ connection setup in main.go to initialize messaging for order service.

--- order-service/main.go
+++ order-service/main.go
@@ -7,24 +7,33 @@ import (
 	"github.com/vaanskii/ecommerce-microservices/order-service/middleware"
 	pb "github.com/vaanskii/ecommerce-microservices/order-service/proto"
 	"github.com/vaanskii/ecommerce-microservices/order-service/services"
+	"github.com/vaanskii/ecommerce-microservices/order-service/utils"
 	"google.golang.org/grpc"
 )
 
 func main() {
- listener, err := net.Listen("tcp", ":50052")
- if err != nil {
-     log.Fatalf("Failed to listen on port 50052: %v", err)
- }
+	conn, ch, err := utils.ConnectToRabbitMQ()
+	if err != nil {
+		log.Fatalf("cannot connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	defer ch.Close()
 
- grpcServer := grpc.NewServer(
+
+	listener, err := net.Listen("tcp", ":50052")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 50052: %v", err)
+	}
+
+	grpcServer := grpc.NewServer(
 	grpc.UnaryInterceptor(middleware.UnaryAuthInterceptor),
- )
+	)
 
- orderService := &services.OrderServiceServer{}
- pb.RegisterOrderServiceServer(grpcServer, orderService)
+	orderService := &services.OrderServiceServer{}
+	pb.RegisterOrderServiceServer(grpcServer, orderService)
 
- log.Println("Order service is running on port 50052...")
- if err := grpcServer.Serve(listener); err != nil {
-     log.Fatalf("Failed to serve: %v", err)
- }
+	log.Println("Order service is running on port 50052...")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
