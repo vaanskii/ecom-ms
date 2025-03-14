7cbbc54c29086e2d8be3b6355a6bc3cc17b40806
Giorgi Vanadze <giorgivanadze03@gmail.com>
2025-03-14 11:22:59 +0400

    Create go func in order-service/main.go - Implement go func which trying to run the queue declare func if RabbitMQ server is down till it will reconnect. - It will not stop gRPC server to run.

--- order-service/main.go
+++ order-service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"time"
 
 	"github.com/vaanskii/ecommerce-microservices/order-service/middleware"
 	pb "github.com/vaanskii/ecommerce-microservices/order-service/proto"
@@ -13,6 +14,18 @@ import (
 
 func main() {
 	utils.ConnectToRabbitMQ()
+
+	go func() {
+		for {
+			err := utils.DeclareQueue("order_created")
+			if err != nil {
+                log.Printf("Warning: Failed to declare RabbitMQ queue: %v. Retrying...", err)
+                time.Sleep(5 * time.Second)
+                continue
+            }
+            break
+		}
+	}()
 
 	listener, err := net.Listen("tcp", ":50052")
 	if err != nil {
